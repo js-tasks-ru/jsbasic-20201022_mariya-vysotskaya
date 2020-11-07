5a0ab8a6d674bcafcfd4a49a2dd97c3d6459ab6d
mariavysotskaya <vysotskaya92@gmail.com>
2020-11-08 00:59:17 +0300

    factorial function created

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+    let result = 1;
+    if (n === 0 || n === 1){
+      return result;
+    } else {
+      for (let i = 0; i < n; i++) {
+        result *= (n - i);
+      }
+    }
+    return result;
 }
